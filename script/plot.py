8cef998fb665fc49fc49d53dcec7e560aea9c981
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-12-13 12:37:02 -0500

    Fix plot to run on small, big seperately

--- script/plot.py
+++ script/plot.py
@@ -3,120 +3,162 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib
 import sys
-
+from keyname import keyname as kn
 matplotlib.rcParams['pdf.fonttype'] = 42
 sns.set(style='whitegrid')
 
 # import dataset
-data = pd.read_csv(sys.argv[1])
+data_in = pd.read_csv(sys.argv[1])
 
-data["Update"] = data["update"]
-data["Problem Difficulty"] = data["confused"]
-data["Best Fitness"] = data["max_fitness"]
-data["Upper Quartile Fitness"] = data["upper_quartile_fitness"]
-data["Selector"] = data["selector"]
+data_in["Update"] = data_in["update"]
+data_in["Problem Difficulty"] = data_in["confused"]
+data_in["Best Fitness"] = data_in["max_fitness"]
+data_in["Upper Quartile Fitness"] = data_in["upper_quartile_fitness"]
+data_in["Selector"] = data_in["selector"]
 
-# BARPLOT ######################################################################
+for size in "big", "small":
 
-sns.barplot(
-    data=data[data["Update"] == data["Update"].max()-1],
-    x="Problem Difficulty",
-    y="Best Fitness",
-    hue="Selector",
-)
+    data_fil = data_in[data_in["size"] == size]
 
-# save to disk
-plt.savefig("depo-consensus-barplot.pdf")
+    # BARPLOT ##################################################################
 
-plt.clf()
+    sns.barplot(
+        data=data_fil[data_fil["Update"] == data_fil["Update"].max()-1],
+        x="Problem Difficulty",
+        y="Best Fitness",
+        hue="Selector",
+    )
 
-sns.barplot(
-    data=data[data["Update"] == data["Update"].max()],
-    x="Problem Difficulty",
-    y="Best Fitness",
-    hue="Selector",
-)
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-barplot",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
 
-# save to disk
-plt.savefig("depo-consensus-barplot-secondary.pdf")
+    plt.clf()
 
-plt.clf()
+    sns.barplot(
+        data=data_fil[data_fil["Update"] == data_fil["Update"].max()],
+        x="Problem Difficulty",
+        y="Best Fitness",
+        hue="Selector",
+        ax=ax1
+    )
 
-# SWARMPLOT ####################################################################
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-barplot-secondary",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
 
-sns.swarmplot(
-    data=data[data["Update"] == data["Update"].max()-1],
-    x="Problem Difficulty",
-    y="Best Fitness",
-    hue="Selector",
-)
+    plt.clf()
 
-# save to disk
-plt.savefig("depo-consensus-swarmplot.pdf")
+    # SWARMPLOT ################################################################
 
-plt.clf()
+    sns.swarmplot(
+        data=data_fil[data_fil["Update"] == data_fil["Update"].max()-1],
+        x="Problem Difficulty",
+        y="Best Fitness",
+        hue="Selector",
+    )
 
-sns.swarmplot(
-    data=data[data["Update"] == data["Update"].max()],
-    x="Problem Difficulty",
-    y="Best Fitness",
-    hue="Selector",
-)
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-swarmplot",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
 
-# save to disk
-plt.savefig("depo-consensus-swarmplot-secondary.pdf")
+    plt.clf()
 
-plt.clf()
+    sns.swarmplot(
+        data=data_fil[data_fil["Update"] == data_fil["Update"].max()],
+        x="Problem Difficulty",
+        y="Best Fitness",
+        hue="Selector",
+    )
 
-# COUNTPLOT ####################################################################
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-swarmplot-secondary",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
 
-sns.countplot(
-    data=data[
-            (data["Update"] == data["Update"].max()-1)
-            & (data["Best Fitness"] == 1)
+    plt.clf()
+
+    # COUNTPLOT ################################################################
+
+    sns.countplot(
+        data=data_fil[
+                (data_fil["Update"] == data_fil["Update"].max()-1)
+                & (data_fil["Best Fitness"] == 1)
+            ],
+        x="Problem Difficulty",
+        hue="Selector",
+    )
+
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-countplot",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
+
+    plt.clf()
+
+    sns.countplot(
+        data=data_fil[
+                (data_fil["Update"] == data_fil["Update"].max())
+                & (data_fil["Best Fitness"] == 1)
+            ],
+        x="Problem Difficulty",
+        hue="Selector",
+    )
+
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-countplot-secondary",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
+
+    plt.clf()
+
+    # LINEPLOT #################################################################
+
+
+    g = sns.FacetGrid(
+        data=data_fil[
+            (data_fil["Update"].mod(100) == 0)
+            & (data_fil["Update"] < data_fil["Update"].max()-1)
         ],
-    x="Problem Difficulty",
-    hue="Selector",
-)
+        col="Problem Difficulty",
+        row="Selector",
+        hue="Selector",
+        margin_titles=True,
+    )
 
-# save to disk
-plt.savefig("depo-consensus-countplot.pdf")
+    g.map(
+        sns.lineplot,
+        "Update",
+        "Upper Quartile Fitness",
+    )
 
-plt.clf()
+    # save to disk
+    outfile = kn.pack({
+        "size" : size,
+        "title" : "depo-consensus-lineplot",
+        "ext" : ".pdf",
+    })
+    plt.savefig(outfile)
 
-sns.countplot(
-    data=data[
-            (data["Update"] == data["Update"].max())
-            & (data["Best Fitness"] == 1)
-        ],
-    x="Problem Difficulty",
-    hue="Selector",
-)
-
-# save to disk
-plt.savefig("depo-consensus-countplot-secondary.pdf")
-
-plt.clf()
-
-# LINEPLOT #####################################################################
-
-
-g = sns.FacetGrid(
-    data=data[
-        (data["Update"].mod(100) == 0)
-        & (data["Update"] < data["Update"].max()-1)
-    ],
-    col="Problem Difficulty",
-    row="Selector",
-    hue="Selector",
-    margin_titles=True,
-)
-
-g.map(
-    sns.lineplot,
-    "Update",
-    "Upper Quartile Fitness",
-)
-
-# save to disk
-plt.savefig("depo-consensus-lineplot.pdf")
+    plt.clf()
